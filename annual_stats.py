02c6ab5eb21115d6592f56246abe0cc0c917f21d
Damien Allen <dev@dallen.co>
2023-10-26 23:07:39 +0200

    LZW compression

--- annual_stats.py
+++ annual_stats.py
@@ -6,7 +6,6 @@ from tqdm import tqdm
 
 clipped_data_dir = Path("data/clipped")
 annual_data_dir = Path("data/annual")
-annual_data_dir.mkdir(exist_ok=True)
 
 clipped_images = [f for f in clipped_data_dir.glob("*.tif")]
 
@@ -19,10 +18,17 @@ largest_meta = None
 for image_path in tqdm(clipped_images, desc="Finding largest extent"):
     with rasterio.open(image_path) as src:
         surface_temp = src.read()
-        if (imgage_size := (src.height * src.width)) > max_size:
-            max_size = imgage_size
+        if (image_size := (src.height * src.width)) > max_size:
+            max_size = image_size
             largest_bounds = src.bounds
             largest_meta = src.meta.copy()
+
+            largest_meta.update(
+                dtype=rasterio.uint8,
+                height=int(surface_temp.shape[1]),
+                width=int(surface_temp.shape[2]),
+                compress="lzw",
+            )
 
 
 # Calculate max surface temp
@@ -42,6 +48,7 @@ for image_path in tqdm(clipped_images, desc="Calculating max surface temp"):
         )
 
 
+annual_data_dir.mkdir(exist_ok=True)
 for year, max_temp in tqdm(max_surface_temp.items(), desc="Writing surface temp"):
     with rasterio.open(
         annual_data_dir / f"max_surface_temp_{year}.tif", "w", **largest_meta
