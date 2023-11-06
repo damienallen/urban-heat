14e5dcbe7e83eae898e3208649e7fe5d11d12bc8
Damien Allen <dev@dallen.co>
2023-11-06 22:52:30 +0100

    Added MapLibre scaffolding

---
+++ viz/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+import { Map } from './Map'
+
+function App() {
+    return (
+        <div className="app">
+            <Map />
+        </div>
+    )
+}
+
+export default App
---
+++ viz/src/Map.tsx
@@ -0,0 +1,29 @@
+import 'maplibre-gl/dist/maplibre-gl.css'
+import './Map.css'
+
+import { useEffect, useRef } from 'react'
+
+import maplibregl from 'maplibre-gl'
+
+const API_KEY = 'bk2NyBkmsa6NdxDbxXvH'
+export const Map = () => {
+    const mapContainer = useRef(null)
+    const map = useRef<null | maplibregl.Map>(null)
+
+    useEffect(() => {
+        if (map.current) return // stops map from intializing more than once
+
+        map.current = new maplibregl.Map({
+            container: mapContainer.current || '',
+            style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
+            center: [4.478, 51.924],
+            zoom: 14,
+        })
+    })
+
+    return (
+        <div className="map-wrap">
+            <div ref={mapContainer} className="map" />
+        </div>
+    )
+}
