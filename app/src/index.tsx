d8af375d7229787c15e9eda6ac301af3eebb1f69
Damien Allen <dev@dallen.co>
2024-03-22 16:04:38 +0100

    Reorganize into components module

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,6 +1,6 @@
 import './index.css'
 
-import { App } from './App.tsx'
+import { App } from './components/App.tsx'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
