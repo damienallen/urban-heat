8b600eda1f1bdbb815b929dd1780312a566611d8
Damien Allen <dev@dallen.co>
2024-03-23 18:55:08 +0100

    Added comlink

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,11 @@
+import { comlink } from 'vite-plugin-comlink'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react(), comlink()],
+    worker: {
+        plugins: [comlink()],
+    },
 })
