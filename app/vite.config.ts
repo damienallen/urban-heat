bb55c4a1015ca160019f165594ef185ee14666c5
Damien Allen <dev@dallen.co>
2024-03-23 21:03:13 +0100

    Upgraded vite

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,6 +6,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react(), comlink()],
     worker: {
-        plugins: [comlink()],
+        plugins: () => [comlink()],
     },
 })
