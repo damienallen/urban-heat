7a130522d79b85e612c9e55f14722a154317f88c
Damien Allen <dev@dallen.co>
2024-03-24 20:37:14 +0100

    Run on host 0.0.0.0

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: { host: '0.0.0.0' },
     plugins: [react(), comlink()],
     worker: {
         plugins: () => [comlink()],
