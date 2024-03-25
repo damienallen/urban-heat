c134ec23d04d31439848e978e4b1ca0c46d8a019
Damien Allen <dev@dallen.co>
2024-03-25 23:51:36 +0100

    Split tooltip into new component

--- app/src/components/ActionBar.tsx
+++ app/src/components/ActionBar.tsx
@@ -5,14 +5,11 @@ import {
     PiStackSimpleDuotone,
 } from 'react-icons/pi'
 
-import { ReactNode } from 'react'
 import { Search } from './Search'
+import { Tooltip } from './Tooltip'
 import { createUseStyles } from 'react-jss'
 
 const containerBgColor = 'rgba(255, 255, 255, 0.9)'
-
-const tooltipBgColor = 'rgba(0, 0, 0, 0.6)'
-const tooltipWidth = 80
 
 const useStyles = createUseStyles({
     container: {
@@ -25,7 +22,7 @@ const useStyles = createUseStyles({
         border: '1px solid rgba(0, 0, 0, 0.2)',
         borderRadius: 8,
         userSelect: 'none',
-        zIndex: 500,
+        zIndex: 400,
     },
     icon: {
         margin: '0 8px',
@@ -34,58 +31,7 @@ const useStyles = createUseStyles({
         flex: 1,
         // cursor: 'pointer',
     },
-    tooltip: {
-        position: 'relative',
-        display: 'flex',
-        '& .tooltip-text': {
-            visibility: 'hidden',
-            width: tooltipWidth,
-            top: '100%',
-            left: '50%',
-            marginTop: 12,
-            marginLeft: -tooltipWidth / 2 /* Half of the width */,
-            backgroundColor: tooltipBgColor,
-            color: '#fff',
-            textAlign: 'center',
-            fontSize: '0.9em',
-            padding: '5px 0',
-            borderRadius: 8,
-            position: 'absolute',
-            zIndex: 1,
-            opacity: 0,
-            transition: 'opacity 0.5s',
-        },
-        '& .tooltip-text::after': {
-            content: '" "',
-            position: 'absolute',
-            bottom: '100%',
-            left: '50%',
-            marginLeft: -5,
-            borderWidth: 5,
-            borderStyle: 'solid',
-            borderColor: `transparent transparent ${tooltipBgColor} transparent`,
-        },
-        '&:hover .tooltip-text': {
-            visibility: 'visible',
-            opacity: 1,
-        },
-    },
 })
-
-interface TooltipProps {
-    children: ReactNode
-    text: string
-}
-
-export const Tooltip = (props: TooltipProps) => {
-    const classes = useStyles()
-    return (
-        <div className={classes.tooltip}>
-            {props.children}
-            <span className="tooltip-text">{props.text}</span>
-        </div>
-    )
-}
 
 export const ActionBar = () => {
     const classes = useStyles()
