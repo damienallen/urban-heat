ae728d7205b521a0f310b4c1f3a0fd9062ba6d61
Damien Allen <dev@dallen.co>
2024-03-31 13:09:58 +0200

    Show thresholds in legend

--- app/src/components/Legend.tsx
+++ app/src/components/Legend.tsx
@@ -1,46 +1,78 @@
 import { PiSlidersDuotone } from 'react-icons/pi'
-import { Tooltip } from './Tooltip'
 import { createUseStyles } from 'react-jss'
+import { observer } from 'mobx-react'
+import { useStores } from '../stores'
 
 const useStyles = createUseStyles({
     container: {
         background: '#fff',
         display: 'flex',
-        alignItems: 'center',
-        flex: '0 1',
-        width: '100vw',
         padding: '8px 0',
         borderTop: '1px solid rgba(0, 0, 0, 0.2)',
         userSelect: 'none',
         zIndex: 400,
         '@media (min-width: 720px)': {
-            flex: '0 1 420px',
+            flex: 0,
             margin: 16,
             padding: 8,
-            background: 'rgba(255, 255, 255, 0.8)',
             border: '1px solid rgba(0, 0, 0, 0.2)',
             borderRadius: 8,
         },
     },
     icon: {
-        margin: '0 8px',
         fontSize: '1.8em',
         color: '#666',
-        flex: 1,
+        flex: '0 0 32px',
         cursor: 'pointer',
         '@media (max-width: 720px)': {
             fontSize: '1.6em',
         },
     },
+    items: {
+        margin: '0 8px 0 16px',
+        display: 'flex',
+        flex: 1,
+        gap: 16,
+    },
+    threholdItem: {
+        flex: 1,
+        display: 'flex',
+        gap: 8,
+    },
+    thresholdColor: {
+        flex: 0,
+        minWidth: 32,
+        background: '#f00',
+        borderRadius: 8,
+    },
+    thresholdText: {
+        flex: 1,
+    },
+})
+
+export const LegendItems = observer(() => {
+    const { app } = useStores()
+    const classes = useStyles()
+
+    let items = []
+    for (let ind = 0; ind < app.contourThresholds.length; ind++) {
+        items.push(
+            <div key={`threshold-${ind}`} className={classes.threholdItem}>
+                <div className={classes.thresholdColor} style={{ opacity: 0.2 * (ind + 1) }} />
+                <div className={classes.thresholdText}>{app.contourThresholds[ind]}°C</div>
+            </div>
+        )
+    }
+
+    return <div className={classes.items}>{items}</div>
 })
 
 export const Legend = () => {
     const classes = useStyles()
     return (
         <div className={classes.container}>
-            <Tooltip text="Settings">
-                <PiSlidersDuotone className={classes.icon} />
-            </Tooltip>
+            <PiSlidersDuotone className={classes.icon} />
+            <LegendItems />
         </div>
     )
 }
