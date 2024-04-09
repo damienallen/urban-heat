5d1e5c322f3c9ba355b9a773cc72ae6a44ca4177
Damien Allen <dev@dallen.co>
2024-04-09 23:12:47 +0200

    Added CORS middleware

--- service/urban_heat/main.py
+++ service/urban_heat/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from urban_heat.sources import urban_extents
 
 app = FastAPI()
+
+origins = [
+    "https://urbanheat.app",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
