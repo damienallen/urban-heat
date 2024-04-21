798d65602ec216b361910808431763136cf5783b
Damien Allen <dev@dallen.co>
2024-04-21 17:57:18 +0000

    Updated download script

--- scripts/pipelines/download.py
+++ scripts/pipelines/download.py
@@ -1,59 +1,89 @@
 import os
-
+from pathlib import Path
 import geopandas as gpd
 import httpx
 from tqdm import tqdm
 
 from pipelines import app_dir
 
-service_url = "https://m2m.cr.usgs.gov/api/api/json/stable"
+SERVICE_URL = "https://m2m.cr.usgs.gov/api/api/json/stable"
+MAX_RESULTS = 100
 
-r = httpx.post(
-    f"{service_url}/login-token",
-    json={
-        "username": os.environ.get("EROS_USERNAME"),
-        "token": os.environ.get("EROS_PASSWORD"),
-    },
-)
-
-r.raise_for_status()
-api_key = r.json()["data"]
-headers = {"X-Auth-Token": api_key}
+MAX_CLOUD_COVER = 80
 
 urban_extents_path = app_dir / "public" / "urban_extents.geojson"
 extents: gpd.GeoDataFrame = gpd.read_file(urban_extents_path)
 
-total_scenes = 0
-scenes = []
-for _, urau in tqdm(extents.iterrows(), desc="Searching scenes", total=extents.shape[0]):
-    spatialFilter = {
-        "filterType": "mbr",
-        "lowerLeft": {"latitude": urau.geometry.bounds[3], "longitude": urau.geometry.bounds[0]},
-        "upperRight": {"latitude": urau.geometry.bounds[1], "longitude": urau.geometry.bounds[2]},
-    }
 
-    # TODO: cloud cover
-    payload = {
-        "datasetName": "landsat_ot_c2_l2",
-        "maxResults": 1,
-        "startingNumber": 0,
-        "sceneFilter": {
-            "spatialFilter": spatialFilter,
-            "acquisitionFilter": {"start": "2013-01-01", "end": "2023-12-31"},
+def find_scenes(query: dict, headers: dict, offset: int = 0):
+    """
+    TODO: cloud cover
+    """
+    url = f"{SERVICE_URL }/scene-search"
+    print(f"[POST] {url} | {offset=}")
+
+    r = httpx.post(
+        url,
+        json={
+            "datasetName": "landsat_ot_c2_l2",
+            "maxResults": MAX_RESULTS,
+            "startingNumber": offset,
+            "sceneFilter": query,
         },
-    }
+        headers=headers,
+    )
 
-    r = httpx.post(f"{service_url }/scene-search", json=payload, headers=headers)
+    scenes = []
+    if r.json()["data"]["totalHits"] > MAX_RESULTS and offset < r.json()["data"]["totalHits"]:
+        scenes += find_scenes(query, headers, offset + MAX_RESULTS)
 
-    total_scenes += r.json()["data"]["totalHits"]
-
-    # for scene in r.json()["data"]["results"]:
-    #     scenes.append(scene["displayId"])
+    return scenes + r.json()["data"]["results"]
 
 
-# scenes_set = set(scenes)
-# duplicate_scenes = len(scenes) - len(scenes_set)
-# percent_duplicate = duplicate_scenes / tot
+def download_urau(
+    code: str, data_dir: Path, start_date: str = "2013-01-01", end_date: str = "2023-12-31"
+):
+    # print("[AUTH] Logging into EROS M2M")
+    # r = httpx.post(
+    #     f"{SERVICE_URL}/login-token",
+    #     json={
+    #         "username": os.environ.get("EROS_USERNAME"),
+    #         "token": os.environ.get("EROS_PASSWORD"),
+    #     },
+    # )
 
-# print(f"Found {len(scenes)} after discarding {duplicate_scenes} duplicates")
-print(f"Found {total_scenes} scenes")
+    # r.raise_for_status()
+    # api_key = r.json()["data"]
+    # headers = {"X-Auth-Token": api_key}
+
+    # urau_extent = extents[extents["URAU_CODE"] == code]
+    # if urau_extent.empty:
+    #     raise ValueError(f"Unable to find are with code '{code}'")
+
+    # scene_filter = {
+    #     "spatialFilter": {
+    #         "filterType": "mbr",
+    #         "lowerLeft": {
+    #             "latitude": urau_extent.geometry.bounds.miny.values[0],
+    #             "longitude": urau_extent.geometry.bounds.minx.values[0],
+    #         },
+    #         "upperRight": {
+    #             "latitude": urau_extent.geometry.bounds.maxy.values[0],
+    #             "longitude": urau_extent.geometry.bounds.maxx.values[0],
+    #         },
+    #     },
+    #     "acquisitionFilter": {"start": start_date, "end": end_date},
+    # }
+
+    # print(f"Finding scenes with {code=}")
+    # urau_scenes = find_scenes(scene_filter, headers)
+
+    existing_ids = [s for s in data_dir.glob("*.TIF")]
+    # display_ids = [s["displayId"] for s in urau_scenes if s["cloudCover"] < MAX_CLOUD_COVER]
+    # print(f"Found {len(urau_scenes)} scenes")
+
+    print("Done")
+
+
+if __name__ == "__main__":
+    download_urau(code="NL037C", data_dir=Path("/home/damien/code/urban-heat/data/raw"))
