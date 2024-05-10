f276aa0a6750180856434a5e1157271b1f9eafda
Damien Allen <dev@dallen.co>
2024-05-10 21:32:14 +0200

    Added tinydb

--- service/tasks/__init__.py
+++ service/tasks/__init__.py
@@ -1,8 +1,12 @@
 import os
 from pathlib import Path
 
+from tinydb import TinyDB
+
 DATA_DIR = Path(__file__).parents[2] / "data"
 APP_DIR = Path(__file__).parents[2] / "app"
 
-DOWNLOADS_YAML_PATH = DATA_DIR / "downloads.yaml"
 DOWNLOADS_DIR = Path(os.environ.get("UH_DOWNLOADS_DIR", "/home/damien/cave/heat_maps/raw/"))
+
+
+db = TinyDB(DATA_DIR / "downloads.json")
