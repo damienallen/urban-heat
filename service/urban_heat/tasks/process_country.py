e45d8a471ffa8268bdda7ce4b167ace1590570b0
Damien Allen <dev@dallen.co>
2024-05-14 23:05:19 +0200

    Updated clipping script to project to WGS84 and cache extent lookups

--- service/urban_heat/tasks/process_country.py
+++ service/urban_heat/tasks/process_country.py
@@ -7,43 +7,53 @@ import numpy as np
 import rasterio
 import typer
 from pydantic import BaseModel, ConfigDict
+from rasterio.enums import Resampling
 from rasterio.mask import mask
+from rasterio.warp import calculate_default_transform, reproject
 from shapely.geometry import Polygon
+from tinydb import Query, TinyDB
 from tqdm import tqdm
 from tqdm.contrib.concurrent import process_map
 
 from urban_heat.tasks import (
     CLIPPED_DIR,
+    DATA_DIR,
     DOWNLOADS_DIR,
     get_extents_by_country,
 )
 
 NO_DATA = 0
+DST_CRS = "EPSG:4326"
+
+SCENCES_JSON = DATA_DIR / "scenes_by_country.json"
+db = TinyDB(SCENCES_JSON)
+Countries = Query()
+
+
+class SceneMetadata(BaseModel):
+    capture_date: str
+    path: str
+    crs: str
+
+
+class CountryScenes(BaseModel):
+    code: str
+    scenes: list[SceneMetadata]
 
 
 class ClippingScene(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
     code: str
-    scene: dict
+    metadata: SceneMetadata
     mask_gdf: gpd.GeoDataFrame
 
 
 def clip_scene(clipping_scene: ClippingScene):
-    image_path = clipping_scene.scene["path"]
+    image_path = Path(clipping_scene.metadata.path)
 
     with rasterio.open(image_path) as src:
         masked_data, masked_transform = mask(src, clipping_scene.mask_gdf.geometry, invert=False)
-
-        dst_meta = src.meta.copy()
-        dst_meta.update(
-            dtype=rasterio.uint8,
-            height=int(masked_data.shape[1]),
-            width=int(masked_data.shape[2]),
-            nodata=NO_DATA,
-            transform=masked_transform,
-            compress="lzw",
-        )
 
         # Apply scale factor and convert to celcius
         # https://www.usgs.gov/faqs/how-do-i-use-a-scale-factor-landsat-level-2-science-products
@@ -56,9 +66,33 @@ def clip_scene(clipping_scene: ClippingScene):
         clipped_images_dir.mkdir(exist_ok=True)
 
         clipped_image_path = clipped_images_dir / f"{image_path.stem}.tif"
+
+        # Transform to WGS84
+        transform, width, height = calculate_default_transform(
+            src.crs, DST_CRS, src.width, src.height, *src.bounds
+        )
+
+        dst_meta = src.meta.copy()
+        dst_meta.update(
+            dtype=rasterio.uint8,
+            height=int(masked_data.shape[1]),
+            width=int(masked_data.shape[2]),
+            nodata=NO_DATA,
+            crs=DST_CRS,
+            transform=transform,
+            compress="lzw",
+        )
+
         with rasterio.open(clipped_image_path, "w", **dst_meta) as dst:
-            dst.write(temp_c)
-            dst.write(temp_c)
+            reproject(
+                source=temp_c,
+                destination=rasterio.band(dst, 1),
+                src_transform=masked_transform,
+                src_crs=src.crs,
+                dst_transform=transform,
+                dst_crs=DST_CRS,
+                resampling=Resampling.nearest,
+            )
 
 
 def clip_country_scenes(country_code: str, downloads_dir: Path = DOWNLOADS_DIR):
@@ -67,50 +101,69 @@ def clip_country_scenes(country_code: str, downloads_dir: Path = DOWNLOADS_DIR):
     mask_gdf_utm = {}
 
     # Fetch and sort list of raw Level-2 LST images
-    images = []
-    for image_path in tqdm([f for f in DOWNLOADS_DIR.glob("*.TIF")], desc="Matching extents"):
-        with rasterio.open(image_path) as src:
-            if src.crs not in mask_gdf_utm:
-                mask_gdf_utm[src.crs] = mask_gdf.to_crs(src.crs)
+    query = db.search((Countries.code == country_code))
+    if len(query):
+        print("Using cached scenes.")
+        country_scenes = CountryScenes(**query[0])
 
-            image_bounds_gdf = gpd.GeoDataFrame(
-                geometry=[
-                    Polygon(
-                        (
-                            (src.bounds.left, src.bounds.top),
-                            (src.bounds.right, src.bounds.top),
-                            (src.bounds.right, src.bounds.bottom),
-                            (src.bounds.left, src.bounds.bottom),
-                            (src.bounds.left, src.bounds.top),
+        for image_path in tqdm(
+            [Path(s.path) for s in country_scenes.scenes], desc="Making CRS masks"
+        ):
+            with rasterio.open(image_path) as src:
+                if (src_crs := str(src.crs)) not in mask_gdf_utm:
+                    mask_gdf_utm[src_crs] = mask_gdf.to_crs(src_crs)
+
+    else:
+        image_metadata = []
+        for image_path in tqdm([f for f in downloads_dir.glob("*.TIF")], desc="Matching extents"):
+            with rasterio.open(image_path) as src:
+                if (src_crs := str(src.crs)) not in mask_gdf_utm:
+                    mask_gdf_utm[src_crs] = mask_gdf.to_crs(src_crs)
+
+                image_bounds_gdf = gpd.GeoDataFrame(
+                    geometry=[
+                        Polygon(
+                            (
+                                (src.bounds.left, src.bounds.top),
+                                (src.bounds.right, src.bounds.top),
+                                (src.bounds.right, src.bounds.bottom),
+                                (src.bounds.left, src.bounds.bottom),
+                                (src.bounds.left, src.bounds.top),
+                            )
                         )
-                    )
-                ],
-                crs=src.crs,
+                    ],
+                    crs=src.crs,
+                )
+
+            intersection = mask_gdf_utm[src.crs].overlay(image_bounds_gdf, how="intersection")
+            if intersection.empty:
+                continue
+
+            image_metadata.append(
+                SceneMetadata(
+                    code=country_code,
+                    capture_date=date(
+                        year=int(image_path.stem[26:30]),
+                        month=int(image_path.stem[30:32]),
+                        day=int(image_path.stem[32:34]),
+                    ).strftime("%Y-%m-%d"),
+                    path=str(image_path),
+                    crs=str(src.crs),
+                )
             )
 
-        intersection = mask_gdf_utm[src.crs].overlay(image_bounds_gdf, how="intersection")
-        if intersection.empty:
-            continue
-
-        images.append(
-            {
-                "capture_date": date(
-                    year=int(image_path.stem[26:30]),
-                    month=int(image_path.stem[30:32]),
-                    day=int(image_path.stem[32:34]),
-                ),
-                "path": image_path,
-                "crs": src.crs,
-            }
-        )
+        image_metadata = sorted(image_metadata, key=lambda img: img.capture_date)  # type: ignore
+        country_scenes = CountryScenes(code=country_code, scenes=image_metadata)
+        db.insert(country_scenes.model_dump())
 
     # Multi-core clipping
-    images = sorted(images, key=lambda img: img["capture_date"])  # type: ignore
     process_map(
         clip_scene,
         [
-            ClippingScene(code=country_code, scene=scene, mask_gdf=mask_gdf_utm[scene["crs"]])
-            for scene in images
+            ClippingScene(
+                code=country_code, metadata=metadata, mask_gdf=mask_gdf_utm[metadata.crs]
+            )
+            for metadata in country_scenes.scenes
         ],
         max_workers=os.cpu_count(),
         chunksize=1,
