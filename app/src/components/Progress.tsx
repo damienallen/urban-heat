768e7bb5eccd014cbcb75212f0b3c9ff418fb393
Damien Allen <dev@dallen.co>
2024-05-25 10:39:23 +0200

    Simplified loader

--- app/src/components/Progress.tsx
+++ app/src/components/Progress.tsx
@@ -1,4 +1,4 @@
-import { RingProgress } from '@mantine/core'
+import { Loader } from '@mantine/core'
 import { createUseStyles } from 'react-jss'
 import { observer } from 'mobx-react'
 import { useStores } from '../stores'
@@ -9,9 +9,6 @@ const useStyles = createUseStyles({
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-    },
-    ring: {
-        flex: 1,
     },
     state: {
         flex: 1,
@@ -28,14 +25,7 @@ export const Progress = observer((props: ProgressProps) => {
 
     return (
         <div className={classes.container}>
-            <RingProgress
-                className={classes.state}
-                rootColor="#ccc"
-                sections={[{ value: ui.loadingProgress, color: props.color }]}
-                size={72}
-                thickness={8}
-                roundCaps
-            />
+            <Loader size={64} color="#fff" type="dots" />
             <div className={classes.state} style={{ color: props.color }}>
                 {ui.loadingState}...
             </div>
