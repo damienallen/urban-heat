8d7ebcef8a8d02e773dfb327e9f03a1f2dfe4695
Damien Allen <dev@dallen.co>
2024-06-04 18:42:32 +0200

    Added sitemap page

--- app/src/router.tsx
+++ app/src/router.tsx
@@ -1,7 +1,12 @@
 import { App } from './components/App.tsx'
+import { SiteMap } from './components/SiteMap.tsx'
 import { createBrowserRouter } from 'react-router-dom'
 
 export const router = createBrowserRouter([
+    {
+        path: '/sitemap',
+        element: <SiteMap />,
+    },
     {
         path: '*',
         element: <App />,
